a5b0e20a1bff13488b6db23e03e46b21f6c22245
omar-dulaimi <o.m.dulaimi@gmail.com>
2022-05-18 03:15:37 +0300

    add default format options

--- src/utils/formatFile.ts
+++ src/utils/formatFile.ts
@@ -3,13 +3,22 @@ import prettier from 'prettier';
 export const formatFile = (content: string): Promise<string> => {
   return new Promise((res, rej) =>
     prettier.resolveConfig(process.cwd()).then((options) => {
+      let formatOptions = options;
       if (!options) {
-        res(content); // no prettier config was found, no need to format
+        formatOptions = {
+          trailingComma: 'all',
+          tabWidth: 2,
+          printWidth: 80,
+          bracketSpacing: true,
+          semi: true,
+          singleQuote: true,
+          useTabs: false,
+        };
       }
 
       try {
         const formatted = prettier.format(content, {
-          ...options,
+          ...formatOptions,
           parser: 'typescript',
         });
 
