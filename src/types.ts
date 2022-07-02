8af5ef7adc2876213e2d0098a7813c9a3880537c
Stire <stire.ru@gmail.com>
2022-07-03 01:18:45 +0300

    moved prisma import / jsonschema implementation to separate functions + TransformerParams to types.ts

---
+++ src/types.ts
@@ -0,0 +1,8 @@
+import { DMMF as PrismaDMMF } from '@prisma/client/runtime';
+
+export type TransformerParams = {
+  enumTypes?: PrismaDMMF.SchemaEnum[];
+  fields?: PrismaDMMF.SchemaArg[];
+  name?: string;
+  modelOperations?: PrismaDMMF.ModelMapping[];
+};
