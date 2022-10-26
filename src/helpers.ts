303dc514714e438789ca9b56d54c1c7bca6d6065
Roy <pawit2540@hotmail.com>
2022-10-26 19:29:08 +0700

    add aggregate input type

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,2 +1,5 @@
 export const isMongodbRawOp = (name: string) =>
   /find([^]*?)Raw/.test(name) || /aggregate([^]*?)Raw/.test(name);
+
+export const isAggregateOutputType = (name: string) =>
+  /(?:Count|Avg|Sum|Min|Max)AggregateOutputType$/.test(name);
