56639f23d268f460747c85fb7727646660f1d622
Konrad Juszkiewicz <kodezen@protonmail.com>
2022-10-28 15:00:49 -0400

    Refactored generate logic into dedicated functions

--- src/prisma-generator.ts
+++ src/prisma-generator.ts
@@ -1,123 +1,114 @@
-import { EnvValue, GeneratorOptions } from '@prisma/generator-helper';
+import {
+  DMMF,
+  EnvValue,
+  GeneratorConfig,
+  GeneratorOptions,
+} from '@prisma/generator-helper';
 import { getDMMF, parseEnvValue } from '@prisma/internals';
 import { promises as fs } from 'fs';
 import Transformer from './transformer';
 import removeDir from './utils/removeDir';
+import {
+  // addMissingInputObjectTypes,
+  handleMongoDbRawOperationsAndQueries,
+} from './helpers';
 
 export async function generate(options: GeneratorOptions) {
-  const outputDir = parseEnvValue(options.generator.output as EnvValue);
-  await fs.mkdir(outputDir, { recursive: true });
-  await removeDir(outputDir, true);
+  await handleGeneratorOutputValue(options.generator.output as EnvValue);
 
-  const prismaClientProvider = options.otherGenerators.find(
-    (it) => parseEnvValue(it.provider) === 'prisma-client-js',
+  const prismaClientGeneratorConfig = getGeneratorConfigByProvider(
+    options.otherGenerators,
+    'prisma-client-js',
   );
 
   const prismaClientDmmf = await getDMMF({
     datamodel: options.datamodel,
-    previewFeatures: prismaClientProvider?.previewFeatures,
+    previewFeatures: prismaClientGeneratorConfig?.previewFeatures,
   });
+
+  checkForCustomPrismaClientOutputPath(prismaClientGeneratorConfig);
+
+  await generateEnumSchemas(
+    prismaClientDmmf.schema.enumTypes.prisma,
+    prismaClientDmmf.schema.enumTypes.model ?? [],
+  );
+
+  const models = prismaClientDmmf.datamodel.models;
+  const modelOperations = prismaClientDmmf.mappings.modelOperations;
+  const inputObjectTypes = prismaClientDmmf.schema.inputObjectTypes.prisma;
+  const outputObjectTypes = prismaClientDmmf.schema.outputObjectTypes.prisma;
 
   const dataSource = options.datasources?.[0];
-
-  Transformer.isDefaultPrismaClientOutput =
-    prismaClientProvider?.isCustomOutput ?? false;
-
-  if (prismaClientProvider?.isCustomOutput) {
-    Transformer.prismaClientOutputPath =
-      prismaClientProvider?.output?.value ?? '';
-  }
-
-  Transformer.setOutputPath(outputDir);
-
-  const enumTypes = [
-    ...prismaClientDmmf.schema.enumTypes.prisma,
-    ...(prismaClientDmmf.schema.enumTypes.model ?? []),
-  ];
-  const enumNames = enumTypes.map((enumItem) => enumItem.name);
-  Transformer.enumNames = enumNames ?? [];
-  const enumsObj = new Transformer({
-    enumTypes,
-  });
-  await enumsObj.printEnumSchemas();
-
-  const inputObjectTypes = prismaClientDmmf.schema.inputObjectTypes.prisma;
-  const rawOpsMap: { [name: string]: string } = {};
-
-  /* 
-  TODO: remove once Prisma fix this issue: https://github.com/prisma/prisma/issues/14900
-  */
-  if (dataSource.provider === 'mongodb') {
-    const modelNames = prismaClientDmmf.mappings.modelOperations.map(
-      (item) => item.model,
-    );
-    const rawOpsNames = [
-      ...new Set(
-        prismaClientDmmf.mappings.modelOperations.reduce<string[]>(
-          (result, current) => {
-            const keys = Object.keys(current);
-            keys?.forEach((key) => {
-              if (key.includes('Raw')) {
-                result.push(key);
-              }
-            });
-            return result;
-          },
-          [],
-        ),
-      ),
-    ];
-
-    rawOpsNames.forEach((opName) => {
-      modelNames.forEach((modelName) => {
-        const isFind = opName === 'findRaw';
-        const opWithModel = `${opName.replace('Raw', '')}${modelName}Raw`;
-        rawOpsMap[opWithModel] = isFind
-          ? `${modelName}FindRawArgs`
-          : `${modelName}AggregateRawArgs`;
-      });
-    });
-
-    Transformer.rawOpsMap = rawOpsMap ?? {};
-
-    const queryOutputTypes =
-      prismaClientDmmf.schema.outputObjectTypes.prisma.filter(
-        (item) => item.name === 'Query',
-      );
-
-    const mongodbRawQueries =
-      queryOutputTypes?.[0].fields.filter((field) =>
-        field.name.includes('Raw'),
-      ) ?? [];
-
-    mongodbRawQueries.forEach((item) => {
-      inputObjectTypes.push({
-        name: item.name,
-        constraints: {
-          maxNumFields: null,
-          minNumFields: null,
-        },
-        fields: item.args.map((arg) => ({
-          name: arg.name,
-          isRequired: arg.isRequired,
-          isNullable: arg.isNullable,
-          inputTypes: arg.inputTypes,
-        })),
-      });
-    });
-  }
-
   Transformer.provider = dataSource.provider;
 
+  // TODO: remove once Prisma fix this issue: https://github.com/prisma/prisma/issues/14900
+  if (dataSource.provider === 'mongodb') {
+    handleMongoDbRawOperationsAndQueries(
+      modelOperations,
+      outputObjectTypes,
+      inputObjectTypes,
+    );
+  }
+
+  addMissingInputObjectTypes(inputObjectTypes, outputObjectTypes, models);
+  await generateObjectSchemas(inputObjectTypes);
+
+  await generateModelSchemas(modelOperations);
+}
+
+async function handleGeneratorOutputValue(generatorOuputValue: EnvValue) {
+  const outputDirectoryPath = parseEnvValue(generatorOuputValue);
+
+  // create the output directory and delete contents that might exist from a previous run
+  await fs.mkdir(outputDirectoryPath, { recursive: true });
+  const isRemoveContentsOnly = true;
+  await removeDir(outputDirectoryPath, isRemoveContentsOnly);
+
+  Transformer.setOutputPath(outputDirectoryPath);
+}
+
+function getGeneratorConfigByProvider(
+  generators: GeneratorConfig[],
+  provider: string,
+) {
+  return generators.find((it) => parseEnvValue(it.provider) === provider);
+}
+
+function checkForCustomPrismaClientOutputPath(
+  prismaClientGeneratorConfig: GeneratorConfig | undefined,
+) {
+  if (prismaClientGeneratorConfig?.isCustomOutput) {
+    Transformer.setPrismaClientOutputPath(
+      prismaClientGeneratorConfig.output?.value as string,
+    );
+  }
+}
+
+async function generateEnumSchemas(
+  prismaSchemaEnum: DMMF.SchemaEnum[],
+  modelSchemaEnum: DMMF.SchemaEnum[],
+) {
+  const enumTypes = [...prismaSchemaEnum, ...modelSchemaEnum];
+  const enumNames = enumTypes.map((enumItem) => enumItem.name);
+  Transformer.enumNames = enumNames ?? [];
+  const transformer = new Transformer({
+    enumTypes,
+  });
+  await transformer.generateEnumSchemas();
+}
+
+async function generateObjectSchemas(inputObjectTypes: DMMF.InputType[]) {
   for (let i = 0; i < inputObjectTypes.length; i += 1) {
     const fields = inputObjectTypes[i]?.fields;
     const name = inputObjectTypes[i]?.name;
-    const obj = new Transformer({ name, fields });
-    await obj.printObjectSchemas();
+    const transformer = new Transformer({ name, fields });
+    await transformer.generateObjectSchema();
   }
+}
 
-  const obj = new Transformer({
-    modelOperations: prismaClientDmmf.mappings.modelOperations,
+async function generateModelSchemas(modelOperations: DMMF.ModelMapping[]) {
+  const transformer = new Transformer({
+    modelOperations,
   });
-  await obj.printModelSchemas();
+  await transformer.generateModelSchemas();
 }
