d50716909ff23bf83d1de63d64fea7f60c5d3758
Roy <pawit2540@hotmail.com>
2022-11-08 21:20:51 +0700

    Fix bug aggregate when Model has only string

--- src/helpers/aggregate-helpers.ts
+++ src/helpers/aggregate-helpers.ts
@@ -3,6 +3,13 @@ import { DMMF } from '@prisma/generator-helper';
 const isAggregateOutputType = (name: string) =>
   /(?:Count|Avg|Sum|Min|Max)AggregateOutputType$/.test(name);
 
+export const isAggreateInputType = (name: string) =>
+  name.endsWith('CountAggregateInput') ||
+  name.endsWith('SumAggregateInput') ||
+  name.endsWith('AvgAggregateInput') ||
+  name.endsWith('MinAggregateInput') ||
+  name.endsWith('MaxAggregateInput');
+
 export function addMissingInputObjectTypesForAggregate(
   inputObjectTypes: DMMF.InputType[],
   outputObjectTypes: DMMF.OutputType[],
@@ -29,4 +36,57 @@ export function addMissingInputObjectTypesForAggregate(
       })),
     });
   }
+}
+
+export type AggregateOperationSupport = {
+  [model: string]: {
+    count?: boolean;
+    min?: boolean;
+    max?: boolean;
+    sum?: boolean;
+    avg?: boolean;
+  };
+};
+
+export function resolveAggregateOperationSupport(
+  inputObjectTypes: DMMF.InputType[],
+) {
+  const aggregateOperationSupport: AggregateOperationSupport = {};
+  for (const inputType of inputObjectTypes) {
+    if (isAggreateInputType(inputType.name)) {
+      const name = inputType.name.replace('AggregateInput', '');
+      if (name.endsWith('Count')) {
+        const model = name.replace('Count', '');
+        aggregateOperationSupport[model] = {
+          ...aggregateOperationSupport[model],
+          count: true,
+        };
+      } else if (name.endsWith('Min')) {
+        const model = name.replace('Min', '');
+        aggregateOperationSupport[model] = {
+          ...aggregateOperationSupport[model],
+          min: true,
+        };
+      } else if (name.endsWith('Max')) {
+        const model = name.replace('Max', '');
+        aggregateOperationSupport[model] = {
+          ...aggregateOperationSupport[model],
+          max: true,
+        };
+      } else if (name.endsWith('Sum')) {
+        const model = name.replace('Sum', '');
+        aggregateOperationSupport[model] = {
+          ...aggregateOperationSupport[model],
+          sum: true,
+        };
+      } else if (name.endsWith('Avg')) {
+        const model = name.replace('Avg', '');
+        aggregateOperationSupport[model] = {
+          ...aggregateOperationSupport[model],
+          avg: true,
+        };
+      }
+    }
+  }
+  return aggregateOperationSupport;
 }
--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,12 @@
 import { DMMF as PrismaDMMF } from '@prisma/client/runtime';
+import { AggregateOperationSupport } from './helpers/aggregate-helpers';
 
 export type TransformerParams = {
   enumTypes?: PrismaDMMF.SchemaEnum[];
   fields?: PrismaDMMF.SchemaArg[];
   name?: string;
   modelOperations?: PrismaDMMF.ModelMapping[];
+  aggregateOperationSupport?: AggregateOperationSupport;
   isDefaultPrismaClientOutput?: boolean;
   prismaClientOutputPath?: string;
 };
