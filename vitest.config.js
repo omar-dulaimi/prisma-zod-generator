957b328e03e9afe8565897f0680d446e0670c3a3
omar-dulaimi <o.m.dulaimi@gmail.com>
2025-08-10 06:13:08 +0300

    chore: send prisma generate logs to console; test: disable watch mode for vitest runs

--- vitest.config.js
+++ vitest.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    watch: false,
     globals: true,
     environment: 'node',
     include: ['tests/**/*.{test,spec}.ts'],
